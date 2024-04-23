c4d889ca490b7fa070a1c2e90a9fd60a5104882e
Kauã Vinícius <106679339+KauaVargas@users.noreply.github.com>
2024-04-23 08:03:05 -0300

    Aula 24/04

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll ('.botao');
+const botoes = document.querySelectorAll('.botao');
 const textos = document.querySelectorAll('.aba-conteudo');
 
 const contadores = document.querySelectorAll('.contador');
@@ -10,16 +10,25 @@ const tempoObjetivo04 = new Date("2024-08-25T00:00:00");
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-for (let i=0; i<tempos.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i])
+function atualizaCronometro() {
+    for (let i = 0; i < tempos.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i])
+    }
 }
 
-let tempoAtual = new Date ()
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
 
-for(let i=0; i<botoes.length; i++){
-    botoes[i].onclick = function(){
+//comecaCronometro();
 
-        for(j=0; j<botoes.length; j++){
+let tempoAtual = new Date()
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
         }
@@ -29,20 +38,23 @@ for(let i=0; i<botoes.length; i++){
     }
 }
 
-function calculaTempo (tempoObjetivo) {
-    
+function calculaTempo(tempoObjetivo) {
+
     let tempoAtual = new Date
     let tempoFinal = tempoObjetivo - tempoAtual
 
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dia = Math.floor(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dia = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-
+    
+    if(tempoFinal > 0){
     return dia + " Dias " + horas + " Horas" + minutos + " Minutos " + segundos + " Segundos "
-
+    } else {
+        return "Prazo finalizado"
+    }
 }
