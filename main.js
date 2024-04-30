33fc86d96505f5d8154599b6db58894e56f1114f
Kauã Vinícius <106679339+KauaVargas@users.noreply.github.com>
2024-04-30 08:52:18 -0300

    Aula 30/04

--- main.js
+++ main.js
@@ -10,22 +10,8 @@ const tempoObjetivo04 = new Date("2024-08-25T00:00:00");
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-function atualizaCronometro() {
-    for (let i = 0; i < tempos.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i])
-    }
-}
-
-function comecaCronometro() {
-    atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
-}
-
-//comecaCronometro();
-
-let tempoAtual = new Date()
-
 for (let i = 0; i < botoes.length; i++) {
+
     botoes[i].onclick = function () {
 
         for (j = 0; j < botoes.length; j++) {
@@ -39,22 +25,42 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 function calculaTempo(tempoObjetivo) {
-
     let tempoAtual = new Date
-    let tempoFinal = tempoObjetivo - tempoAtual
+    let tempoFinal = tempoObjetivo - tempoAtual;
 
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
-    let dia = Math.floor(horas / 24);
+    let dias = Math.floor(horas / 24);
+
+
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
-    if(tempoFinal > 0){
-    return dia + " Dias " + horas + " Horas" + minutos + " Minutos " + segundos + " Segundos "
+
+    if (tempoFinal > 0) {
+        return [dias, horas, minutos, segundos];
     } else {
-        return "Prazo finalizado"
+        return [0, 0, 0, 0];
     }
 }
+
+function atualizaCronometro() {
+
+    for (let i = 0; i < tempos.length; i++) {
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("segs"+i).textContent = calculaTempo(tempos[i])[3];
+    }
+}
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
+
+let tempoAtual = new Date()
